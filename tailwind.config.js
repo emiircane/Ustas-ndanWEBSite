1dcb49d992b1f5edfddb545684384c7d7a148671
Emir <emircanergen@hotmail.com>
2025-04-20 22:35:16 +0300

    Tailwind config fix + animasyon ayarları

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  // tailwind.config.js
-module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       keyframes: {
